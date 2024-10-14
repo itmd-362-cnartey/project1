58cf32ba088aa54223e70ab3d66449dbce801f35
cyetran <156265100+cyetran@users.noreply.github.com>
2024-10-14 08:29:41 -0500

    Update site.js
    added missing team form control

--- js/site.js
+++ js/site.js
@@ -7,6 +7,7 @@ form.addEventListener('submit', function(event){
 	const lastname = document.getElementByID('lastname').value;
 	const email = document.getElementByID('email').value;
 	const key = document.getElementByID('key').value;
+	const team = document.getElementByID('team').value;
 	const teamA = document.getElementByID('teamA').checked;
 	const teamB = document.getElementById('teamB').checked;
 	const teamC = document.getElementById('teamC').checked;
